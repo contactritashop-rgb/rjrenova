cb604e9230039e53c61cd858e1916a329e9e4a34
CodeWords Library User <codewordslibrary@agemo.ai>
2026-07-28 00:30:00 +0000

    Simplify chatbot wrapper

--- components/chatbot/chatbot-wrapper.tsx
+++ components/chatbot/chatbot-wrapper.tsx
@@ -1,10 +1,4 @@
 "use client";
 
-import dynamic from "next/dynamic";
-
-const Chatbot = dynamic(() => import("@/components/chatbot/chatbot").then((m) => ({ default: m.Chatbot })), { ssr: false });
-
-export function ChatbotWrapper() {
-  return <Chatbot />;
-}
+export { Chatbot } from "./chatbot";
 
